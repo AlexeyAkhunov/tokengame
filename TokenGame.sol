b9561c04e87e46196e7f27ec91943d527c97011a
Alexey Akhunov <alexeyakhunov@Alexeys-Mac-Pro.home>
2017-05-14 23:15:09 +0100

    Initial commit of the token game

---
+++ TokenGame.sol
@@ -0,0 +1,136 @@
+pragma solidity ^0.4.11;
+
+contract Token {
+	uint256 public totalSupply;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) {
+        require(balanceOf[msg.sender] >= _value);            // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]);  // Check for overflows
+        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+
+    /* Allow another contract to spend some tokens in your behalf */
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    /* A contract attempts to get the coins */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        require(balanceOf[_from] >= _value);                 // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]);  // Check for overflows
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        balanceOf[_from] -= _value;                          // Subtract from the sender
+        balanceOf[_to] += _value;                            // Add the same to the recipient
+        allowance[_from][msg.sender] -= _value;
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    uint public id; /* To ensure distinct contracts for different tokens owned by the same owner */
+    address public owner;
+    bool public sealed = false;
+
+    function Token(uint _id) {
+        owner = msg.sender;
+        id = _id;
+    }
+
+    /* Allows the owner to mint more tokens */
+    function mint(address _to, uint256 _value) returns (bool) {
+    	require(msg.sender == owner);
+    	require(!sealed);
+    	balanceOf[_to] += _value;
+    	totalSupply += _value;
+    	return true;
+    }
+
+    function seal() {
+    	require(msg.sender == owner);
+    	sealed = true;
+    }
+}
+
+contract Withdraw {
+	Token public token;
+
+	function Withdraw(Token _token) {
+		token = _token;
+	}
+
+	function () payable {}
+
+	function withdraw() {
+		require(token.sealed());
+		require(token.balanceOf(msg.sender) > 0);
+		uint amount = token.balanceOf(msg.sender);
+		if (!token.transferFrom(msg.sender, this, amount) || !msg.sender.send(amount)) {
+			throw;
+		}
+	}
+}
+
+contract TokenGame {
+	address public owner;
+	uint public cap_in_wei;
+	uint constant initial_duration = 7 days;
+	uint constant time_extension_from_doubling = 7 days;
+	uint constant time_of_half_decay = 7 days;
+	uint256 constant fixed_point_multiplier = 2**128;
+	Token public excess_token; /* Token contract used to receive excess after the sale */
+	Withdraw excess_withdraw;  /* Withdraw contract distributing the excess */
+ 	Token public game_token;   /* Token contract used to receive prizes */
+ 	uint public end_time; 	   /* Current end time */
+ 	uint last_time = 0;        /* Timestamp of the latest contribution */
+ 	uint256 ema = 0;           /* Current value of the EMA */
+ 	uint total_wei_given = 0;  /* Total amount of wei given via fallback function */
+
+	function TokenGame(uint _cap_in_wei) {
+		owner = msg.sender;
+		cap_in_wei = _cap_in_wei;
+		excess_token = Token(1);
+		excess_withdraw = Withdraw(excess_token);
+		game_token = Token(2);
+		end_time = now + initial_duration;
+	}
+
+	function () payable {
+		require(now <= end_time);	// Check that the sale has not ended
+		require(msg.value > 0);     // Check that something has been sent
+		total_wei_given += msg.value;
+		uint decay_factor = time_of_half_decay * fixed_point_multiplier / (time_of_half_decay + (now - last_time) );
+		ema = msg.value * fixed_point_multiplier + decay_factor * ema;
+		uint time_extension = ema * time_extension_from_doubling / total_wei_given / fixed_point_multiplier;
+		if (now + time_extension > end_time) {
+			end_time = now + time_extension;
+		}
+		if (!excess_token.mint(msg.sender, msg.value) || !game_token.mint(msg.sender, msg.value)) {
+			throw;
+		}
+	}
+
+	function finalise() {
+		require(now > end_time);
+		excess_token.seal();
+		game_token.seal();
+		if (this.balance > cap_in_wei) {
+			if (!excess_withdraw.send(this.balance - cap_in_wei) || !owner.send(cap_in_wei)) {
+				throw;
+			}
+		} else {
+			if (!owner.send(this.balance)) {
+				throw;
+			}
+		}
+	}
+}
